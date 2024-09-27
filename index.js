2a3c493376b9c8a8646a8b49fbcbef2bba4618a4
akash-harale1 <haraleakash3@gmail.com>
2024-09-27 08:55:52 +0530

    Update index.js to demonstrate viewing all books

--- index.js
+++ index.js
@@ -10,30 +10,7 @@ const book2 = new Book("456", "Clean Code", "Robert Martin", 2009);
 library.addBook(book1);
 library.addBook(book2);
 
-// Borrowing a book
-try {
-  library.borrowBook("123");
-  console.log(`${book1.title} has been borrowed.`);
-} catch (error) {
-  console.error(error.message);
-}
-
-// Viewing available books
 console.log(
-  "Available books:",
-  library.viewAvailableBooks().map((book) => book.toString())
-);
-
-// Returning a book
-try {
-  library.returnBook("123");
-  console.log(`${book1.title} has been returned.`);
-} catch (error) {
-  console.error(error.message);
-}
-
-// Final available books
-console.log(
-  "Final available books:",
-  library.viewAvailableBooks().map((book) => book.toString())
+  "All books in the library:",
+  library.viewAllBooks().map((book) => book.toString())
 );
